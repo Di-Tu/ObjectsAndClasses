de69e7d986ee5d9f637207f03448bc9ab4f747f2
Ди <faraon_m@mail.ru>
2025-07-27 21:44:36 +0300

    Homework2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        System.out.println("Создание");
         Author oscarWilde = new Author("Oscar", "Wilde");
         Book thePictureOfDorianGray = new Book("The Picture of Dorian Gray", oscarWilde, 2024);
         System.out.println("Название книги: " + thePictureOfDorianGray.getNameBook() + ", Автор: " + thePictureOfDorianGray.getAuthorBookName().getName() + " " + thePictureOfDorianGray.getAuthorBookName().getSurname() + ", год: " + thePictureOfDorianGray.getYearBook());
@@ -8,7 +9,27 @@ public class App {
         Book freeShooter = new Book("Вольный стрелок", konstantinFedorov, 2021);
         System.out.println("Название книги: " + freeShooter.getNameBook() + ", Автор: " + freeShooter.getAuthorBookName().getName() + " " + freeShooter.getAuthorBookName().getSurname() + ", год: " + freeShooter.getYearBook());
 
+        Author konstantinFedorov2 = new Author("Константин", "Федоров");
+        Book freeShooter2 = new Book("Вольный стрелок", konstantinFedorov2, 2020);
+        System.out.println("Название книги: " + freeShooter2.getNameBook() + ", Автор: " + freeShooter2.getAuthorBookName().getName() + " " + freeShooter2.getAuthorBookName().getSurname() + ", год: " + freeShooter2.getYearBook());
+
+        System.out.println();
+        System.out.println("Изменение");
         freeShooter.setYearBook(2020);
         System.out.println("Название книги: " + freeShooter.getNameBook() + ", Автор: " + freeShooter.getAuthorBookName().getName() + " " + freeShooter.getAuthorBookName().getSurname() + ", год: " + freeShooter.getYearBook());
+
+        System.out.println();
+        System.out.println("Вывод toString");
+        System.out.println(konstantinFedorov.toString());
+        System.out.println(freeShooter.toString());
+
+        System.out.println();
+        System.out.println("Сравнение книг");
+        if (freeShooter.hashCode() != freeShooter2.hashCode()) {
+            System.out.println("Книги разные");
+        } else if (freeShooter.equals(freeShooter2)) {
+            System.out.println("Одна и та же книга");
+        } else System.out.println("Книги разные");
+
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private Author authorBookName;
@@ -23,5 +25,22 @@ public class Book {
 
     public void setYearBook(int year) {
         this.yearBook = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + nameBook + ", Автор: " + authorBookName.toString() + ", год издания: " + yearBook;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearBook == book.yearBook && Objects.equals(authorBookName, book.authorBookName) && nameBook.equals(book.nameBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, authorBookName.hashCode(), yearBook);
     }
 }
